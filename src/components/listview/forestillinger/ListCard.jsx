2d5a7c46ee38b02ce141219aae41cfb2f68330cd
Mariawillow <mani0013@stud.kea.dk>
2025-12-02 21:36:56 +1100

    fjernet link

--- src/components/listview/forestillinger/ListCard.jsx
+++ src/components/listview/forestillinger/ListCard.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
 import AnchorTagPrimaryButton from "@/components/global/knapper/AnchorTagPrimaryButton";
 
 export default function ListCard({ items = [] }) {
