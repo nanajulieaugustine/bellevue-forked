f561731c25a9028f3f8894ae2c18aa78134c9bca
Mariawillow <mani0013@stud.kea.dk>
2025-12-04 16:19:25 +1100

    venneforening "done" manlger småting + karrusel

--- src/components/venneforening/VennerKarrusel.jsx
+++ src/components/venneforening/VennerKarrusel.jsx
@@ -1,7 +1,13 @@
 
-
 const VennerKarrusel = () => (
     <div>
+        <div>
+            <h4>Aktuelle Medlemstilbud</h4>
+            <p>Gratis billetter til udvalgte forestillinger</p>
+            <div>
+                <h1>Billede karrusel</h1>
+            </div>
+        </div>
     
     </div>
   );
