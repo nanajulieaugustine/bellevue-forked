a010504f78bcd565e13986868ff715c35a3f3c46
nanajulieaugustine <naje0004@stud.kea.dk>
2025-12-17 13:06:30 +0100

    Nana: merge listfilter

--- src/components/listview/forestillinger/ListFilterClient.jsx
+++ src/components/listview/forestillinger/ListFilterClient.jsx
@@ -8,18 +8,14 @@ export default function ListFilterClient({ items }) {
       items={items}
       showTabs={true}
       onlyFuture={false}
+      allowMultipleTimes={false}
       introTitle="Se vores fulde program"
       introText="Her får du et samlet overblik over alle aktuelle og kommende forestillinger. Sortér efter genre eller målgruppe og find præcis det, der passer til jeres aften i teatret."
       renderCard={(filteredGrouped) => (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {filteredGrouped.flatMap(({ shows }) =>
             shows.map(({ item, time }, i) => (
-              <ListCard
-                key={item.id + "-" + i}
-                item={item}
-                isArchived={false}
-                className="basis-[calc(33.333%-0.5rem)]"
-              />
+              <ListCard key={item.id + "-" + i} item={item} isArchived={false} className="basis-[calc(33.333%-0.5rem)]" />
             ))
           )}
         </div>
